57870314953f48dc1f81a7891bb73c910feac882
Robert Ramey <ramey@rrsd.com>
2006-06-07 03:33:30 +0000

    tweaks to fix borlands problem with strong type
    [SVN r34218]

--- include/boost/archive/basic_text_oarchive.hpp
+++ include/boost/archive/basic_text_oarchive.hpp
@@ -91,12 +91,23 @@ public:
     // text file don't include the optional information 
     void save_override(const class_id_optional_type & /* t */, int){}
 
+    // note the following four overrides are necessary for some borland
+    // compilers which don't handle BOOST_STRONG_TYPE properly.
     void save_override(const version_type & t, int){
         // note:t.t resolves borland ambguity
-        unsigned  char x = t.t;
+        unsigned int x = t.t;
         * this->This() << x;
     }
-
+    void save_override(const class_id_type & t, int){
+        // note:t.t resolves borland ambguity
+        int x = t.t;
+        * this->This() << x;
+    }
+    void save_override(const class_id_reference_type & t, int){
+        // note:t.t resolves borland ambguity
+        int x = t.t;
+        * this->This() << x;
+    }
     void save_override(const class_name_type & t, int){
         const std::string s(t);
         * this->This() << s;
