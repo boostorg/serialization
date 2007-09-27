714480207b6f839d337de22c57d0836d4f6323f7
Robert Ramey <ramey@rrsd.com>
2007-09-27 04:46:21 +0000

    improvements to extended type info system - unfortunately, these break the export.
    [SVN r39570]

---
+++ test/portable_be_binary_archive.hpp
@@ -0,0 +1,11 @@
+// (C) Copyright 2002-4 Robert Ramey - http://www.rrsd.com .
+// Use, modification and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// See http://www.boost.org for updates, documentation, and revision history.
+// file includes for testing a custom archive.
+// as an example this tests the portable binary archive
+
+#include "portable_binary_archive.hpp"
+#define TEST_ARCHIVE_FLAGS endian_big
