d5aaa3801aa40c83b14ce980c077da79a8ced578
Vladimir Prus <vladimir@codesourcery.com>
2015-04-06 12:48:06 +0300

    Revert "fix a couple of gcc warnings"
    This reverts commit 79e73a715c0a323404cdc5f29e2f25dd573473b7.

--- include/boost/archive/iterators/transform_width.hpp
+++ include/boost/archive/iterators/transform_width.hpp
@@ -113,11 +113,10 @@ public:
     transform_width(BOOST_PFTO_WRAPPER(T) start) : 
         super_t(Base(BOOST_MAKE_PFTO_WRAPPER(static_cast< T >(start)))),
         m_buffer_out_full(false),
-        m_buffer_out(),
         // To disable GCC warning, but not truly necessary 
 	    //(m_buffer_in will be initialized later before being 
 	    //used because m_remaining_bits == 0)
-        m_buffer_in(), 
+        m_buffer_in(0), 
         m_remaining_bits(0),
         m_end_of_sequence(false)
     {}
