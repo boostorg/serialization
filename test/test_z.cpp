54a68ca9de38c93b5584eefb0e165fa056761077
Robert Ramey <ramey@rrsd.com>
2015-10-05 21:00:18 -0700

    adding test case

--- test/test_z.cpp
+++ test/test_z.cpp
@@ -1,3 +1,23 @@
-int main() {
-    return 0;
+#include <fstream>
+#include <string>
+
+#include <boost/archive/xml_woarchive.hpp>
+
+#include "test_tools.hpp"
+
+int test_main(int, char *argv[])
+{
+    const char * testfile = boost::archive::tmpnam(NULL);
+	std::wofstream ofs(testfile);
+
+	std::string s1 = "kkk";
+	std::wstring w1 = L"kkk";
+	std::wstring w2 = L"апр"; // some non-lati1 (for example russians) letters
+    {
+        boost::archive::xml_woarchive oa(ofs);
+        oa << boost::serialization::make_nvp("key1", s1);
+        oa << boost::serialization::make_nvp("key2", w1);
+        oa << boost::serialization::make_nvp("key3", w2); // here exception is thrown
+    }
+	return 0;
 }
