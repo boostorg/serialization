0040321a7c62034e5a78efaee4c3bf25470a01c1
Robert Ramey <ramey@rrsd.com>
2016-01-20 22:58:29 -0800

    another attempt to fix codecvt facet

--- src/utf8_codecvt_facet.cpp
+++ src/utf8_codecvt_facet.cpp
@@ -4,9 +4,8 @@
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #define BOOST_ARCHIVE_SOURCE
-#include <boost/serialization/config.hpp>
+#include <boost/config.hpp>
 
-#include <boost/archive/detail/utf8_codecvt_facet.hpp>
 #include <boost/archive/detail/auto_link_archive.hpp>
 
 #ifdef BOOST_NO_STD_WSTREAMBUF
@@ -15,7 +14,7 @@
     #ifdef BOOST_NO_CXX11_HDR_CODECVT
         #define BOOST_UTF8_BEGIN_NAMESPACE \
              namespace boost { namespace archive { namespace detail {
-        #define BOOST_UTF8_DECL
+        #define BOOST_UTF8_DECL BOOST_ARCHIVE_DECL
         #define BOOST_UTF8_END_NAMESPACE }}}
         #include <boost/detail/utf8_codecvt_facet.ipp>
         #undef BOOST_UTF8_END_NAMESPACE
