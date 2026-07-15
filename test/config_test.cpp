f71fa5bd8484418ccf153169d972d3f982a95acf
Gennaro Prota <gennaro.prota@gmail.com>
2026-07-15 16:41:56 +0200

    Add missing license references
    Some files carried no license reference. Add to them the standard
    reference with the correct "Distributed under the Boost Software
    License" wording; the copyright year and holder come from each file's
    first commit.
    
    Closes issue #336.

--- test/config_test.cpp
+++ test/config_test.cpp
@@ -1,3 +1,11 @@
+/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
+// config_test.cpp
+
+// Copyright 2014 Robert Ramey
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+
 #include "../../../boost/config.hpp"
 
 #if defined(__clang__)
